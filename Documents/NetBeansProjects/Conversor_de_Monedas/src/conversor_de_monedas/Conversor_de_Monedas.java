d55be9dfb38492cddeed15252f251ca224247e8a
MendiCe <mendieta_cesar@hotmail.com>
2023-03-16 10:26:59 -0500

    Carga Inicial

---
+++ Documents/NetBeansProjects/Conversor_de_Monedas/src/conversor_de_monedas/Conversor_de_Monedas.java
@@ -0,0 +1,21 @@
+
+package conversor_de_monedas;
+
+import java.util.Scanner;
+import javax.swing.JOptionPane;
+
+public class Conversor_de_Monedas {
+    public static void main(String[] args) {
+        Scanner entradaDatos = new Scanner (System.in);        
+        
+         int cantArray;
+         
+         int [] conjunto = {1,2,3};
+        
+         for (int i = 0;i<3;i++){
+             System.out.println(conjunto[i]);
+         }
+         
+         
+    }
+}
