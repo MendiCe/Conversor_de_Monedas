eed0b9dd7cf287bb4b01553e0531b2ce6331c19a
MendiCe <mendieta_cesar@hotmail.com>
2023-03-17 11:23:42 -0500

    Se definen los tipos de datos y sus argumentos, se agrega cuadro de Bienvenida y el Cuadro de selección del tipo de conversión.

--- Documents/NetBeansProjects/Conversor_de_Monedas/src/conversor_de_monedas/Conversor_de_Monedas.java
+++ Documents/NetBeansProjects/Conversor_de_Monedas/src/conversor_de_monedas/Conversor_de_Monedas.java
@@ -1,6 +1,7 @@
 
 package conversor_de_monedas;
 
+import java.awt.Component;
 import java.util.Scanner;
 import javax.swing.JOptionPane;
 
@@ -8,8 +9,20 @@ public class Conversor_de_Monedas {
     public static void main(String[] args) {
         Scanner entrada = new Scanner(System.in);
         
-        System.out.println("Hola");
+        //Se definen variables y se asignan los valores correspondientes.
+        int seleccionCambio;
+        Component frame = null;
+        String [] seleccionConversor = {" ","Conversor de moneda", "Conversor de temperatura"};
         
+        //Saludo Inicial por utilizar el Software.
+        JOptionPane.showMessageDialog(frame, "Bienvenidos al Conversor Mundial.", "Bienvenidos", JOptionPane.INFORMATION_MESSAGE, null);
+        
+        //Se solicita hacer selección del tipo de conversión que desea realizar.
+        Object opcionElegida = JOptionPane.showInputDialog(frame, "Elija de las opciones: ", "Hacer seleccion"
+                , JOptionPane.INFORMATION_MESSAGE,null, seleccionConversor,null);
+        
+        
+                
     }
 
 }
